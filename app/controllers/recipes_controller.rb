572775739548f9649410edfcf3ed9041098d1c61
Sensei100 <jenanton01@gmail.com>
2017-01-20 19:23:06 +0000

    show like/dislike totals and add validations

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
     
   def index
-    @recipes = Recipe.all    
+    @recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total}.reverse   
   end
   
   def show
@@ -40,9 +40,14 @@ class RecipesController < ApplicationController
   
   def like
     @recipe = Recipe.find(params[:id])
-    Like.create(like: params[:like], chef: Chef.find(3), recipe: @recipe)
-    flash[:success] = "Your selection was successful"
-    redirect_to :back
+    like = Like.create(like: params[:like], chef: Chef.find(3), recipe: @recipe)
+    if like.valid?
+      flash[:success] = "Your selection was successful"
+      redirect_to :back
+    else
+      flash[:danger] = "You can only like/dislike a recipe once"
+      redirect_to :back
+    end
   end
   
   private
