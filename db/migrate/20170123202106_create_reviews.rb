2038ce09c06fdc0630e9fccbcf3e27b465bcf999
Sensei100 <jenanton01@gmail.com>
2017-01-23 20:30:06 +0000

    create migration and model for reviews

---
+++ db/migrate/20170123202106_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.text :content
+      t.integer :chef_id
+      t.integer :recipe_id
+      t.timestamps
+    end
+  end
+end
